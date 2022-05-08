7dd83e5e76822d5ac2a98cc1faa9d96aa3f5d7f3
Aman Patel <amanpatel.102@gmail.com>
2022-05-08 03:04:05 -0400

    Add files via upload

---
+++ Tsavo Webpage/js/scripts.js
@@ -0,0 +1,10 @@
+var count = 1;
+function handleDayChange(number) {
+  count += number;
+  if (count < 1) {
+    count = 1;
+    alert('Minimum 1 day is required.');
+    return;
+  }
+  document.getElementById('number').value = count;
+}
